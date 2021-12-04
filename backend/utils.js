e7f49588f330f0a5b361d5198998168d72ae2d0a
Ilya <vertiprahov.ilya@yandex.ru>
2021-12-04 21:39:14 +0500

    feat: Доработал запись состояния

--- backend/utils.js
+++ backend/utils.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 function saveState(name, state) {
-    fs.writeFile(`./backend/state/${name}.json`, JSON.stringify(state), (error) => {
-        console.log(error);
-    });
+	const stateFile = path.join(__dirname, `./backend/state/${name}.json`);
+    console.log(`Try put state ${stateFile}`)
+    fs.writeFileSync(stateFile, JSON.stringify(state));
 }
 
 exports.saveHouseState = (state) => {
